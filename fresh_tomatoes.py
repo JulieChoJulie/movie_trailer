309f618cd96b0c14d41c9c3ed0c06c70fb422845
JulieChoJulie <h32cho@gmail.com>
2018-07-04 15:02:53 -0400

    added docstrings in media.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -9,16 +9,10 @@ main_page_head = '''
     <title>Julie's Favorite Movies!</title>
 
     <!-- Bootstrap 3 -->
-    <link rel="stylesheet"
-    href=
-    "https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet"
-    href=
-    "https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css
-    /bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0
-    /js/bootstrap.min.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <style type="text/css" media="screen">
 
         .image {
@@ -89,20 +83,16 @@ main_page_head = '''
     </style>
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop,
-         .modal', function (event) {
-            // Remove the src so the player itself gets removed, \
-            as this is the only
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+            // Remove the src so the player itself gets removed, as this is the only
             // reliable way to ensure the video stops playing in IE
             $("#trailer-video-container").empty();
         });
         // Start playing the video whenever the trailer modal is opened
         $(document).on('click', '.movie-tile', function (event) {
             var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' +
-            trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container")
-            .empty().append($("<iframe></iframe>", {
+            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
               'id': 'trailer-video',
               'type': 'text-html',
               'src': sourceUrl,
@@ -112,8 +102,7 @@ main_page_head = '''
 
         // Animate in the movies when the page loads
         $(document).ready(function () {
-          $('.movie-tile').hide().first().show("fast",
-          function showNext() {
+          $('.movie-tile').hide().first().show("fast", function showNext() {
             $(this).next("div").show("fast", showNext);
           });
 
@@ -138,11 +127,8 @@ main_page_content = '''
     <div class="modal" id="trailer">
       <div class="modal-dialog">
         <div class="modal-content">
-          <a href="#" class="hanging-close" \
-          data-dismiss="modal" aria-hidden="true">
-            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WR
-            qOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=
-            s0#w=24&h=24"/>
+          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
+            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
           </a>
           <div class="scale-media" id="trailer-video-container">
           </div>
@@ -155,9 +141,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-            Julie's Favorite Movie Trailers
-            </a>
+            <a class="navbar-brand" href="#">Julie's Favorite Movie Trailers</a>
           </div>
         </div>
       </div>
@@ -171,12 +155,9 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center"
-data-trailer-youtube-id="{trailer_youtube_id}" movieInfo="{movieInfo}"
-data-toggle="modal" data-target="#trailer">
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" movieInfo="{movieInfo}" data-toggle="modal" data-target="#trailer">
     <div class="box" style="position:relative">
-        <img class="poster" src="{poster_image_url}" width="220"
-        height="342" >
+        <img class="poster" src="{poster_image_url}" width="220" height="342" >
         <div class="overlay">
             <div class="text"  style="position:relative">{movieInfo}</div>
         </div>
@@ -191,12 +172,9 @@ def create_movie_tiles_content(movies):
     content = ''
     for movie in movies:
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(r'(?<=v=)[^&#]+',
-                                     movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = youtube_id_match.group(0) \
-            if youtube_id_match else None
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
@@ -212,10 +190,8 @@ def open_movies_page(movies):
     # Create or overwrite the output file
     output_file = open('julie_fave_movies.html', 'w')
 
-    # Replace the placeholder for the movie tiles with the actual
-    # dynamically generated content
-    rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+    # Replace the placeholder for the movie tiles with the actual dynamically generated content
+    rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
@@ -223,5 +199,4 @@ def open_movies_page(movies):
 
     # open the output file in the browser
     url = os.path.abspath(output_file.name)
-    # open in a new tab, if possible
-    webbrowser.open('file://' + url, new=2)
+    webbrowser.open('file://' + url, new=2)  # open in a new tab, if possible
--- media.py
+++ media.py
@@ -5,14 +5,32 @@ import webbrowser
 class Movie:
     Valid_Ratings = ["G", "PG", "PG-13", "R"]
 
-    # init the class, Movie, by intaking 4 inputs excluding self.
     def __init__(self, movie_title, movie_info, poster_image, trailer_youtube):
-        # define class variables using inputs
         self.title = movie_title
         self.movieInfo = movie_info
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        """
+
+        Args:
+            movie_title (str): the title of the movie in string form
+            movie_info (str): it is in html format using <p> tags.
+            poster_image (str): it represents url of the poster of the movie.
+            trailer_youtube (str): it represents url of the youtube trailer.
+
+        """
 
     # open the youtube video.
+    @property
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
+        """
+
+        Args:
+            self : self has four different attr defined by __init__.
+        returns:
+            it returns youtube trailer opens using default web browser by
+            taking self.trailer_youtube_url, which is the fourth arguments
+            when the class is initiated.
+
+        """
